2f7b5f429710793a5ec60d83a5316d0f73443fc7
rick-anderson <riande@microsoft.com>
2015-06-09 09:50:39 -0700

    Added contacts

--- ContactManager/ContactManager/Models/IdentityModels.cs
+++ ContactManager/ContactManager/Models/IdentityModels.cs
@@ -29,5 +29,7 @@ namespace ContactManager.Models
         {
             return new ApplicationDbContext();
         }
+
+        public System.Data.Entity.DbSet<ContactManager.Models.Contact> Contacts { get; set; }
     }
 }
